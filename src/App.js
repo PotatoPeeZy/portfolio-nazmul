7fe706382310302ba1e892f35030d0a7ac413aa0
Nazmul <nazmul.hossain.shishir14@gmail.com>
2023-10-30 21:09:50 +0600

    Removed unnecessary imports in app.js

--- src/App.js
+++ src/App.js
@@ -1,12 +1,6 @@
-import { BrowserRouter, Routes, Route } from "react-router-dom";
+import { BrowserRouter } from "react-router-dom";
 import Navbar from "./Components/Navbar";
 import HomePageTab from "./Pages/HomePageTab";
-import Education from "./Pages/Education";
-import Skills from "./Pages/Skills";
-import PhotoGallery from "./Pages/PhotoGallery";
-import General from "./Pages/General";
-import HomePage from "./Pages/HomePage";
-
 function App() {
   return (
     <div>
